dc6d6f74751789405db9b17ab5925a25cfeab757
Gilad Naaman <gnaaman@drivenets.com>
2023-03-15 21:20:36 +0200

    haha it actually works kinda

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use clap::{Parser, Subcommand};
+use futures::StreamExt;
+use oci_spec::image::{ImageConfiguration, ImageManifest, Descriptor, DescriptorBuilder};
 use rayon::prelude::*;
-use reqwest::{Method, StatusCode, Client};
+use reqwest::{Client, Method, StatusCode};
 use serde::Deserialize;
+use sha2::{Sha256, digest::FixedOutput};
 use std::{
-    collections::{HashMap, HashSet},
+    collections::HashMap,
     fs::File,
-    io::Write,
+    io::{Seek, Write},
     str::FromStr,
+    time::Instant,
 };
 use tar::Archive;
 use tokio::io::AsyncReadExt;
@@ -16,7 +20,7 @@ use oci::*;
 pub mod docker_registry_v2;
 pub mod response_async_reader;
 
-use docker_registry_v2::{ParsedImageReference, ParsedDomain};
+use docker_registry_v2::{ParsedDomain, ParsedImageReference};
 use response_async_reader::ResponseAsyncReader;
 
 const IMAGE_MANIFEST_CONTENT_TYPES: &str = "application/vnd.oci.image.manifest.v1+json,application/vnd.docker.distribution.manifest.v2+json";
@@ -31,6 +35,14 @@ fn bytes_to_human_size(byte_size: u64) -> (f64, &'static str) {
     } else {
         (byte_size as f64, "B")
     }
+}
+
+fn hash_string(src: &str) -> String {
+    let mut hasher = Sha256::default();
+    let _ = hasher.write(src.as_bytes());
+    let digest = hasher.finalize_fixed();
+    let byte_strings: Vec<_> = digest.as_slice().into_iter().map(|b| format!("{b:02x}")).collect();
+    byte_strings.join("")
 }
 
 fn analyze(tar_path: &str) {
@@ -176,7 +188,7 @@ async fn get_auth_token(
     client: reqwest::Client,
     registry: &str,
     repository_namespace: &str,
-    repositories: impl Iterator<Item=&str>,
+    repositories: impl Iterator<Item = &str>,
 ) -> anyhow::Result<Option<AuthToken>> {
     let url = format!("https://{registry}/v2/");
 
@@ -195,10 +207,7 @@ async fn get_auth_token(
 
         let response = client
             .get(auth_url)
-            .query(&[
-                ("service", reg_service),
-                ("scope", scopes.as_str()),
-            ])
+            .query(&[("service", reg_service), ("scope", scopes.as_str())])
             .send()
             .await?
             .text()
@@ -222,7 +231,21 @@ fn get_registry(parsed_domain: &Option<ParsedDomain>) -> (String, &'static str) 
     }
 }
 
-async fn get_manifest(client: Client, url: String, token: Option<&AuthToken>) -> anyhow::Result<oci_spec::image::ImageManifest> {
+#[derive(Clone, Debug)]
+struct ImageManifests {
+    manifest: ImageManifest,
+    raw_manifest: String,
+    config: ImageConfiguration,
+    raw_config: String
+}
+
+async fn get_manifest(
+    client: Client,
+    url_base: String,
+    tag: &str,
+    token: Option<&AuthToken>,
+) -> anyhow::Result<ImageManifests> {
+    let url = format!("{url_base}/manifests/{tag}");
     let mut request = client
         .request(Method::GET, url)
         .header("Accept", IMAGE_MANIFEST_CONTENT_TYPES);
@@ -231,14 +254,37 @@ async fn get_manifest(client: Client, url: String, token: Option<&AuthToken>) ->
         request = request.bearer_auth(&token.token);
     };
 
-    let response = request.send().await;
-    let text = response?.text().await?;
-    let manifest = serde_json::from_str::<oci_spec::image::ImageManifest>(&text);
+    let response = request.send().await?;
+    let status = response.status();
+    let raw_manifest = response.text().await?;
 
-    Ok(manifest?)
+    let manifest = serde_json::from_str::<oci_spec::image::ImageManifest>(&raw_manifest).map_err(|e| {
+        eprintln!("FUCKITY FUCK: {url_base} {tag} {raw_manifest}, {:?}", status);
+        e
+    })?;
+
+    let config_digest = manifest.config().digest();
+    let config_url = format!("{url_base}/blobs/{config_digest}");
+
+    let mut request = client.request(Method::GET, config_url);
+    if let Some(token) = token {
+        request = request.bearer_auth(&token.token);
+    };
+    let response = request.send().await?;
+    let raw_config = response.text().await?;
+    let config = serde_json::from_str::<oci_spec::image::ImageConfiguration>(&raw_config)?;
+
+    let manifests = ImageManifests {
+        manifest,
+        raw_manifest,
+        config,
+        raw_config,
+    };
+    Ok(manifests)
 }
 
 async fn download_a_bunch(images: &[ParsedImageReference]) -> anyhow::Result<()> {
+    let start = Instant::now();
     let mut registries = HashMap::new();
 
     for img_ref in images {
@@ -252,37 +298,208 @@ async fn download_a_bunch(images: &[ParsedImageReference]) -> anyhow::Result<()>
     let client = reqwest::Client::new();
 
     // Get auth tokens for each repository.
-    let token_futures = registries.iter().map(|((registry, namespace), repositories)| {
-        let future = get_auth_token(client.clone(), registry, namespace, repositories.iter().map(|(repo, _tag)| repo.as_str()));
-        async { future.await.map(|token| (registry.clone(), token)) }
-    });
-    let tokens: HashMap<_, _> = futures::future::try_join_all(token_futures).await?.into_iter().collect();
+    let token_futures = registries
+        .iter()
+        .map(|((registry, namespace), repositories)| {
+            let future = get_auth_token(
+                client.clone(),
+                registry,
+                namespace,
+                repositories.iter().map(|(repo, _tag)| repo.as_str()),
+            );
+            async { future.await.map(|token| (registry.clone(), token)) }
+        });
+    let tokens: HashMap<_, _> = futures::future::try_join_all(token_futures)
+        .await?
+        .into_iter()
+        .collect();
 
     // Get manifest for each image
     let manifest_requests = images.iter().map(|img_ref| {
         let (registry, repository_namespace) = get_registry(&img_ref.registry);
 
-        let tag = img_ref.tag.as_ref()
-            .map_or_else(|| "latest".to_string(), |t| t.to_string());
+        let tag = img_ref
+            .tag
+            .as_ref()
+            .map_or_else(|| "latest", |t| &*t.digest.as_ref().unwrap_or(&t.tag));
 
         let url = format!(
             "https://{registry}/v2/{repository_namespace}{}/manifests/{tag}",
             img_ref.repository,
         );
 
+        let url_base = format!(
+            "https://{registry}/v2/{repository_namespace}{}/",
+            img_ref.repository,
+        );
+
         let token = tokens.get(&registry).map(|t| t.as_ref()).flatten();
-        get_manifest(client.clone(), url, token)
+        get_manifest(client.clone(), url_base, tag, token)
     });
     let manifests = futures::future::try_join_all(manifest_requests).await?;
 
     let mut layer_digests = HashMap::new();
-    for (manifest, img_ref) in manifests.iter().zip(images.iter()) {
-        for layer in manifest.layers() {
-            layer_digests.insert(layer.digest().clone(), img_ref.repository.clone());
+    for (manifest_bundle, img_ref) in manifests.iter().zip(images.iter()) {
+        for layer in manifest_bundle.manifest.layers() {
+            layer_digests.insert(layer.digest().clone(), img_ref);
         }
     }
 
-    println!("Basically done");
+    dbg!(&manifests);
+
+    let layer_futures = layer_digests.iter().map(|(digest, img_ref)| {
+        let (registry, repository_namespace) = get_registry(&img_ref.registry);
+
+        let url = dbg!(format!(
+            "https://{registry}/v2/{repository_namespace}{}/blobs/{digest}",
+            img_ref.repository
+        ));
+        let token = tokens.get(&registry).unwrap();
+        let mut request = client
+            .request(Method::GET, url)
+            .header("Accept", IMAGE_MANIFEST_CONTENT_TYPES);
+        if let Some(token) = &token {
+            request = request.bearer_auth(&token.token);
+        }
+
+        async move {
+            eprintln!(">> Starting to download {digest}");
+            let mut file = File::options()
+                .create(true)
+                .write(true)
+                .truncate(true)
+                .read(true)
+                .open(digest)?;
+            let response = request.send().await?;
+            // let reader = ResponseAsyncReader::new(response);
+            // let mut decomp = async_compression::tokio::bufread::GzipDecoder::new(reader);
+
+            // let mut buf = [0u8; 4096 * 16];
+
+            // loop {
+            //     let size = decomp.read(&mut buf).await?;
+            //     if size == 0 {
+            //         break;
+            //     }
+            //     let buf = &buf[..size];
+            //     file.write(buf)?;
+            // }
+            let mut stream = response.bytes_stream();
+            while let Some(chunk) = stream.next().await {
+                file.write(&*chunk?)?;
+            }
+
+            eprintln!(">> Finished downloading {digest}");
+            Ok::<_, anyhow::Error>((digest, file))
+        }
+    });
+    let mut layers: HashMap<_, _> = futures::future::try_join_all(layer_futures)
+        .await?
+        .into_iter()
+        .collect();
+
+    for (manifest_bundle, img_ref) in manifests.iter().zip(images.iter()) {
+        let mut file = File::create(format!("{}.tar", img_ref.repository))?;
+        let mut image_tar = tar::Builder::new(&mut file);
+        for layer in manifest_bundle.manifest.layers() {
+            let digest = layer.digest();
+            let layer_file = layers.get_mut(digest).unwrap();
+
+            layer_file.seek(std::io::SeekFrom::Start(0))?;
+            image_tar.append_file(
+                format!("blobs/sha256/{}", digest.split(':').nth(1).unwrap()),
+                layer_file,
+            )?;
+        }
+
+        // Write image config to blobs
+        let mut header = tar::Header::new_gnu();
+        header.set_size(manifest_bundle.raw_config.len() as u64);
+        header.set_cksum();
+        image_tar.append_data(
+            &mut header,
+            format!(
+                "blobs/sha256/{}",
+                manifest_bundle.manifest.config().digest().split(':').nth(1).unwrap()
+            ),
+            manifest_bundle.raw_config.as_bytes(),
+        )?;
+
+        // Write image manifest to blobs
+        let manifest_digest = hash_string(&manifest_bundle.raw_manifest);
+        let mut header = tar::Header::new_gnu();
+        header.set_size(manifest_bundle.raw_manifest.len() as u64);
+        header.set_cksum();
+        image_tar.append_data(
+            &mut header,
+            format!(
+                "blobs/sha256/{manifest_digest}",
+            ),
+            manifest_bundle.raw_manifest.as_bytes(),
+        )?;
+
+        // Write "oci-layout" file
+        let layout_data = "{\"imageLayoutVersion\":\"1.0.0\"}";
+        let mut header = tar::Header::new_gnu();
+        header.set_size(layout_data.len() as u64);
+        header.set_cksum();
+        image_tar.append_data(&mut header, "oci-layout", layout_data.as_bytes())?;
+
+        /*
+        {
+  "schemaVersion": 2,
+  "manifests": [
+    {
+      "mediaType": "application/vnd.docker.distribution.manifest.v2+json",
+      "size": 954,
+      "digest": "sha256:f34ab33ed0e034163ff180b5caa056fc806dfadeeab27f5a40616af951ae7ab8",
+      "annotations": {
+        "io.containerd.image.name": "pr-registry.dev.drivenets.net/gi_dev_v18_1:18.1.0.199_dev.dev_v18_1",
+        "org.opencontainers.image.ref.name": "18.1.0.199_dev.dev_v18_1"
+      }
+    }
+  ]
+} */
+
+        // Write index.json
+        let ref_name = img_ref.tag.as_ref().map(|t| &*t.tag).unwrap_or("latest").to_string();
+
+        let mut image_name = String::new();
+        if let Some(registry) = &img_ref.registry{
+            image_name.push_str(&registry.to_string());
+            image_name.push('/');
+        }
+        image_name.push_str(&img_ref.repository);
+        image_name.push(':');
+        image_name.push_str(&ref_name);
+        
+        let mut index = oci_spec::image::ImageIndex::default();
+        index.set_manifests(vec![
+            DescriptorBuilder::default()
+                .digest(format!("sha256:{manifest_digest}"))
+                .size(manifest_bundle.raw_manifest.len() as i64)
+                .media_type(manifest_bundle.manifest.media_type().as_ref().unwrap().clone())
+                .annotations(HashMap::from([
+                    ("io.containerd.image.name".into(), image_name),
+                    ("org.opencontainers.image.ref.name".into(), ref_name),
+                ]))
+                .build().unwrap()   
+        ]);
+        let raw_index = serde_json::to_string(&index).unwrap();
+
+        let mut header = tar::Header::new_gnu();
+        header.set_size(raw_index.len() as u64);
+        header.set_cksum();
+        image_tar.append_data(&mut header, "index.json", raw_index.as_bytes())?;
+
+        image_tar.finish()?;
+    }
+
+    for layer in layers.keys() {
+        std::fs::remove_file(layer)?;
+    }
+
+    println!("Basically done {:?}", start.elapsed());
 
     Ok(())
 }
@@ -346,18 +563,22 @@ async fn download(
         let request = request.build()?;
         let response = client.execute(request).await?;
 
-        let reader = ResponseAsyncReader::new(response);
-        let mut decomp = async_compression::tokio::bufread::GzipDecoder::new(reader);
-        let mut buf = [0u8; 4096];
         let mut file = File::create(digest)?;
-        loop {
-            let size = decomp.read(&mut buf).await?;
-            if size == 0 {
-                break;
-            }
-            let buf = &buf[..size];
-            file.write(buf)?;
+        let mut stream = response.bytes_stream();
+        while let Some(chunk) = stream.next().await {
+            file.write(&*chunk?)?;
         }
+        // let reader = ResponseAsyncReader::new(response);
+        // let mut decomp = async_compression::tokio::bufread::GzipDecoder::new(reader);
+        // let mut buf = [0u8; 4096];
+        // loop {
+        //     let size = decomp.read(&mut buf).await?;
+        //     if size == 0 {
+        //         break;
+        //     }
+        //     let buf = &buf[..size];
+        //     file.write(buf)?;
+        // }
     }
     Ok(manifest)
 }
@@ -372,21 +593,33 @@ struct Args {
 #[derive(Subcommand, Debug)]
 enum SubCommand {
     Download { images: Vec<String> },
+    Analyze { tar_file: String },
 }
 
-#[tokio::main]
+#[tokio::main(worker_threads = 4)]
 async fn main() -> anyhow::Result<()> {
     let args = dbg!(Args::parse());
 
     // analyze(&image_tar_name);
     match args.command {
+        SubCommand::Analyze { tar_file } => {
+            analyze(&tar_file);
+        }
         SubCommand::Download { images } => {
             let mut references = Vec::new();
             for img in images {
                 references.push(docker_registry_v2::ParsedImageReference::from_str(&img)?);
             }
 
-            download_a_bunch(&references).await?;
+            let f = download_a_bunch(&references).await;
+
+            match &f {
+                Ok(_) => {}
+                Err(e) => {
+                    dbg!(&e, e.backtrace());
+                }
+            }
+            f?;
             println!("Basically done 2")
         }
     }
